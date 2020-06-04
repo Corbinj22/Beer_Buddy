07f216f8d2f6f1117b95457cbd520f35d2884bec
Justin Corbin <jcorbin2290@gmail.com>
2020-06-04 15:34:09 -0600

    💄 Changes makde to index files to alight with 0 state of app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './Components/App/App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+  <Router>
     <App />
+  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

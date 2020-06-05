04b65a7a5de029075c2b5f5c294a0719ac074e69
Justin Corbin <jcorbin2290@gmail.com>
2020-06-04 20:27:56 -0600

    🎨🔮 initial jsx and css for headimg

---
+++ src/Components/HeadImg/HeadImg.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeadImg.css';
+
+
+const HeadImg = () => {
+
+  return (
+    <div className='img-head-container'>
+    </div>
+  )
+}
+
+export default HeadImg;

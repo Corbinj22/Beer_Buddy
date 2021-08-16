1eabce7ad207569e2de98406ee055eb9e6be0f88
Justin Corbin <jcorbin2290@gmail.com>
2021-08-16 15:29:44 -0400

    toy with new UI design and feel

--- src/Components/ExpandedBeerCard/ExpandedBeerCard.js
+++ src/Components/ExpandedBeerCard/ExpandedBeerCard.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import './ExpandedBeerCard.css'
+import './ExpandedBeerCard.css';
 const ExpandedBeerCard = ({ matchedBeer, setNewAverage }) => {
   let average = Number(matchedBeer.average.toFixed(2));
 
   const makeNewAverage = (event) => {
-    average = Number(((average + Number(event.target.value)) / 2).toFixed(2))
-    return  average
-  }
+    average = Number(((average + Number(event.target.value)) / 2).toFixed(2));
+    return average;
+  };
 
   const submitAverage = (event) => {
-    event.preventDefault()
-    setNewAverage(average)
-  }
+    event.preventDefault();
+    setNewAverage(average);
+  };
 
   return (
-    <section className='expandedBeer-container'>
-      <div className='top-card-container'>
-        <div className='name-an-pic'>
+    <section className="expandedBeer-container">
+      <div className="top-card-container">
+        <div className="name-an-pic">
           <h2>{matchedBeer.name}</h2>
-          <img className='beer-img'alt={matchedBeer.name} src={matchedBeer.image_url}/>
+          <img
+            className="beer-img"
+            alt={matchedBeer.name}
+            src={matchedBeer.image_url}
+          />
         </div>
-        <div className='all-details'>
+        <div className="all-details">
           <h2>Profile</h2>
-          <div className='details-container'>
-            <p className='beer-detail'>ABV: {matchedBeer.abv}</p>
-            <p className='beer-detail'>IBU: {matchedBeer.ibu}</p>
-            <p className='beer-detail'>{matchedBeer.tagline}</p>
-            <p className='beer-detail'>Average Raiting: {matchedBeer.average} stars</p>
-            <p className='beer-detail'>Please Leave A Raiting:</p>
+          <div className="details-container">
+            <p className="beer-detail">ABV: {matchedBeer.abv}</p>
+            <p className="beer-detail">IBU: {matchedBeer.ibu}</p>
+            <p className="beer-detail">Tagline: {matchedBeer.tagline}</p>
+            <p className="beer-detail">
+              Average Raiting: {matchedBeer.average} stars
+            </p>
+            <p className="beer-detail">Please Leave A Raiting:</p>
             <form>
-              <button className='user-rate-btn' onClick={submitAverage} type="click">Submit</button>
-              <select onChange={makeNewAverage} className='user-raiting-select'>
+              <button
+                className="user-rate-btn"
+                onClick={submitAverage}
+                type="click"
+              >
+                Submit
+              </button>
+              <select onChange={makeNewAverage} className="user-raiting-select">
                 <option value={null}>Star Raiting</option>
                 <option value={1}>1</option>
                 <option value={2}>2</option>
@@ -47,11 +59,14 @@ const ExpandedBeerCard = ({ matchedBeer, setNewAverage }) => {
           </div>
         </div>
       </div>
-      <div className='bottom-card-container'>
-        <p className='desciption-text card-text'><span className='description-rule'>Desciption</span>{matchedBeer.description}</p>
+      <div className="bottom-card-container">
+        <p className="desciption-text card-text">
+          <span className="description-rule">Desciption</span>
+          {matchedBeer.description}
+        </p>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default ExpandedBeerCard;
